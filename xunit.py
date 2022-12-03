7fb3dc171a21067366d92ed61f187dbd55d99348
YOSHIRO FUJIMORI <yoshiro.fujimori@gmail.com>
2022-12-03 16:38:39 +0900

    move self.name from WasRun up to TestCase

--- xunit.py
+++ xunit.py
@@ -1,10 +1,10 @@
 class TestCase:
-	pass
+	def __init__(self, name):
+		self.name = name
 
 class WasRun(TestCase):
 	def __init__(self, name):
 		self.wasRun = None
-		self.name = name
 	def run(self):
 		method = getattr(self, self.name)
 		method()
