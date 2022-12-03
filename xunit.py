2c8125914eda936ac0ced15acde5b968c6de6823
YOSHIRO FUJIMORI <yoshiro.fujimori@gmail.com>
2022-12-03 21:06:00 +0900

    add testFailedResult

--- xunit.py
+++ xunit.py
@@ -29,6 +29,8 @@ class WasRun(TestCase):
 		self.log = self.log + "testMethod "
 	def tearDown(self):
 		self.log = self.log + "tearDown "
+	def testBrokenMethod(self):
+		raise Exception
 
 class TestCaseTest(TestCase):
 	def testTemplateMethod(self):
@@ -39,7 +41,12 @@ class TestCaseTest(TestCase):
 		test = WasRun("testMethod")
 		result = test.run()
 		assert("1 run, 0 failed" == result.summary())
+	def testFailedResult(self):
+		test = WasRun("testBrokenMethod")
+		result = test.run()
+		assert("1 run, 1 failed" == result.summary())
 
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
+# TestCaseTest("testFailedResult").run()
