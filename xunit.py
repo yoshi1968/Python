381ec0034e52d167dc9930615d24c67f394bf396
YOSHIRO FUJIMORI <yoshiro.fujimori@gmail.com>
2022-12-03 16:45:11 +0900

    correct WasRun to refer to "name" of parent class

--- xunit.py
+++ xunit.py
@@ -1,13 +1,14 @@
 class TestCase:
 	def __init__(self, name):
 		self.name = name
+	def run(self):
+		method = getattr(self, self.name)
+		method()
 
 class WasRun(TestCase):
 	def __init__(self, name):
 		self.wasRun = None
-	def run(self):
-		method = getattr(self, self.name)
-		method()
+		super().__init__(name)
 	def testMethod(self):
 		self.wasRun = 1
 
