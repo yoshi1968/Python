cb9e19478f403993e9af84f835c8a113f82b7847
YOSHIRO FUJIMORI <yoshiro.fujimori@gmail.com>
2022-12-03 18:23:56 +0900

    remove wasRun flag

--- xunit.py
+++ xunit.py
@@ -10,17 +10,14 @@ class TestCase:
 
 class WasRun(TestCase):
 	def setUp(self):
-		self.wasRun = None
 		self.log = "setUp "
 	def testMethod(self):
-		self.wasRun = 1
 		self.log = self.log + "testMethod "
 
 class TestCaseTest(TestCase):
-	def setUp(self):
-		self.test = WasRun("testMethod")
 	def testTemplateMethod(self):
-		self.test.run()
-		assert("setUp testMethod " == self.test.log)
+		test = WasRun("testMethod")
+		test.run()
+		assert("setUp testMethod " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
