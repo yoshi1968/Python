5884c94ce3064e9bb637f283806523972c595744
YOSHIRO FUJIMORI <yoshiro.fujimori@gmail.com>
2022-12-03 19:07:33 +0900

    increment runCount when test is called

--- xunit.py
+++ xunit.py
@@ -6,15 +6,19 @@ class TestCase:
 	def tearDown(self):
 		pass
 	def run(self):
+		result = TestResult()
+		result.testStarted()
 		self.setUp()
 		method = getattr(self, self.name)
 		method()
 		self.tearDown()
-		return TestResult()
+		return result
 
 class TestResult:
 	def __init__(self):
-		self.runCount = 1
+		self.runCount = 0
+	def testStarted(self):
+		self.runCount = self.runCount + 1
 	def summary(self):
 		return "%d run, 0 failed" % self.runCount
 
